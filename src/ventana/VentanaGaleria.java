a287e14e982677298affbebe825a027dc01fcc43
andreamartinez21 <andreamartinez21@opendeusto.es>
2021-10-23 15:51:03 +0200

    VentanaGaleria funcionalidad añadida

--- src/ventana/VentanaGaleria.java
+++ src/ventana/VentanaGaleria.java
@@ -1,5 +1,48 @@
 package ventana;
 
-public class VentanaGaleria {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaGaleria extends JFrame{
+
+	private Container cp;
+    private JPanel panelPrincipal;
+    private JScrollPane scroll;
+
+    public VentanaGaleria() {
+        
+        cp = this.getContentPane();
+		this.setTitle("Galería de fotos");
+        this.setSize(1000, 600);
+
+		int numFotos = 80; // calculará el número de fotos que haya en la bd
+        
+        panelPrincipal = new JPanel(new GridLayout(numFotos/2, 2));
+        panelPrincipal.setSize(1000, 2000);
+        for (int i = 1; i <= numFotos; i++) {
+            panelPrincipal.add(new JTextField("Foto " + i));
+        }
+
+        scroll = new JScrollPane(panelPrincipal);
+        scroll.setVisible(true);
+        this.add(scroll);
+
+        //this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+		 public void run() {
+		  try {
+		   VentanaGaleria frame = new VentanaGaleria();
+		   frame.setVisible(true);
+		  } catch (Exception e) {
+		   e.printStackTrace();
+		  }
+		 }
+		});
+	}
 }
