76dddb117bc1043f98a9c8dc4f5e8a81aec90245
andreamartinez21 <andreamartinez21@opendeusto.es>
2021-10-24 15:46:25 +0200

    VentanaGaleria implementación imágenes

--- src/ventana/VentanaGaleria.java
+++ src/ventana/VentanaGaleria.java
@@ -1,7 +1,11 @@
 package ventana;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class VentanaGaleria extends JFrame{
 
@@ -9,18 +13,21 @@ public class VentanaGaleria extends JFrame{
     private JPanel panelPrincipal;
     private JScrollPane scroll;
 
-    public VentanaGaleria() {
+    public VentanaGaleria() throws IOException {
         
         cp = this.getContentPane();
 		this.setTitle("Galería de fotos");
         this.setSize(1000, 600);
 
-		int numFotos = 80; // calculará el número de fotos que haya en la bd
+		int numFotos = 4; // calculará el número de fotos que haya en la bd
         
         panelPrincipal = new JPanel(new GridLayout(numFotos/2, 2));
         panelPrincipal.setSize(1000, 2000);
+        
         for (int i = 1; i <= numFotos; i++) {
-            panelPrincipal.add(new JTextField("Foto " + i));
+            BufferedImage image = ImageIO.read(new File("images/barco" +i+ ".jpg"));
+		    JLabel label = new JLabel(new ImageIcon(image));
+            panelPrincipal.add(label);
         }
 
         scroll = new JScrollPane(panelPrincipal);
