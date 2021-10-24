10f3f25074c652ca186fe5f67c8e9c259611d443
mikel andoni mingo <mikelmingo@opendeusto.es>
2021-10-24 21:07:32 +0200

    terminada ventana de actividades y ventana de cantidad de personas

--- src/ventana/VentanaActividades.java
+++ src/ventana/VentanaActividades.java
@@ -1,9 +1,23 @@
 package ventana;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -14,16 +28,36 @@ import javax.swing.JTextField;
 public class VentanaActividades extends JFrame{
 
 	
-	public VentanaActividades() {
-
+	public VentanaActividades() throws IOException {
+		
+		JPanel panelPrincipal = new JPanel();
+		JPanel panelIzquierdo = new JPanel();
+		JPanel panelMedio = new JPanel();
+		JPanel panelDerecho = new JPanel();
+		
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JPanel panel5 = new JPanel();
+		JPanel panel6 = new JPanel();
+		JPanel panel7 = new JPanel();
+		JPanel panel8 = new JPanel();
+		
+		
+		
 		JComboBox<String> combobox = new JComboBox();
 		JLabel label1 = new JLabel("¿Desea alguna actividad? ");
 		JLabel label2 = new JLabel("Cantidad de Personas");
 		JLabel label3 = new JLabel("Tarifa");
 		JButton boton1 = new JButton("Aceptar");
 		JButton boton2 = new JButton("No quiero nada");
-		JTextField txt1 = new JTextField("",2);
+		JButton boton3 = new JButton("Elegir cantidad de personas");
 		JTextField txt2 = new JTextField("",10);
+		
+		BufferedImage imagenBarco = ImageIO.read(new File("images/yate2.jpg"));
+		JLabel label4 = new JLabel(new ImageIcon(imagenBarco));
+		
 		txt2.setText("yyyy/mm/dd");
 		
 		combobox.addItem("Surf");
@@ -46,6 +80,7 @@ public class VentanaActividades extends JFrame{
 		add(panelPrincipal);
 		panelPrincipal.add(panelIzquierdo);
 		panelPrincipal.add(panelMedio);
+		panelPrincipal.add(panelDerecho);
 	
 		panelIzquierdo.add(panel1);
 		panelIzquierdo.add(panel2);
@@ -56,7 +91,7 @@ public class VentanaActividades extends JFrame{
 		panelMedio.add(panel6);
 		panelMedio.add(panel7);
 		
-
+		panelDerecho.add(panel8);
 		
 		panel1.add(label1);
 		panel2.add(combobox);
@@ -65,17 +100,28 @@ public class VentanaActividades extends JFrame{
 		panel4.add(boton2);
 		
 		panel5.add(label2);
-		panel6.add(txt1);
+		panel6.add(boton3);
 		panel7.add(label3);
 		
+		panel8.add(label4);
+		
+		boton3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+			new VentanaPersonas();	
+			}
+		});
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(700, 450);
 		setTitle("Ventana Actividades");
+		pack();
 		setVisible(true);
 		
 		
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		new VentanaActividades();
 	}
