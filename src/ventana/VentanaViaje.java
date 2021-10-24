0ead0882ba10cc14c5251ec1f6342af7f772709c
Ane Sainz Tabares <anesainzt@opendeusto.es>
2021-10-24 23:27:44 +0200

    Avance con la ventana para comprar billetes

--- src/ventana/VentanaViaje.java
+++ src/ventana/VentanaViaje.java
@@ -1,5 +1,7 @@
 package ventana;
 
+
+import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,21 +9,30 @@ import javax.swing.*;
 import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
 
 public class VentanaViaje extends JFrame {
-
+	
 	JLabel viaje;
+	JPanel p1, p2;
 	JRadioButton idaVuelta, soloIda, viajesProgramados;//Programados desde la fecha y la hora de hoy
 	JComboBox origen, destino;
 	JLabel lblNewLabel1, lblNewLabel2, lblNewLabel3;
 	
 	
 	public VentanaViaje() {
-	
-		getContentPane().setLayout(new GridLayout(7, 3));
+				
+		//Panel de la izquierda
+		p1 = new JPanel();
+		p1.setLayout(new GridLayout(4, 1));
 		
 		viaje = new JLabel("Tu viaje empieza aquí");
 		idaVuelta = new JRadioButton("Ida y Vuelta");
 		soloIda = new JRadioButton("Solo ida");
 		viajesProgramados = new JRadioButton("Viajes Largos programados");
+		
+		
+		//Panel de la derecha
+		p2 = new JPanel();
+		p2.setLayout(new GridLayout(4, 1));
+		
 		lblNewLabel1 = new JLabel("Calendario1");
 		lblNewLabel2 = new JLabel("Calendario2");
 		lblNewLabel3 = new JLabel("Calendario3");
@@ -41,58 +52,49 @@ public class VentanaViaje extends JFrame {
 				lblNewLabel1.setVisible(true);
 				lblNewLabel2.setVisible(false);
 				lblNewLabel3.setVisible(false);
-			
-				
 			}
-			
 		});
+		
 		soloIda.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				lblNewLabel1.setVisible(false);
 				lblNewLabel2.setVisible(true);
-				lblNewLabel3.setVisible(false);
-				
+				lblNewLabel3.setVisible(false);	
 			}
-			
 		});
+		
 		viajesProgramados.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				lblNewLabel1.setVisible(false);
 				lblNewLabel2.setVisible(false);
-				lblNewLabel3.setVisible(true);
-			
-				
+				lblNewLabel3.setVisible(true);		
 			}
-			
 		});
 		
 		
-	
+		p1.add(viaje);
+		p1.add(idaVuelta);
+		p1.add(soloIda);
+		p1.add(viajesProgramados);
 		
-		
-		getContentPane().add(viaje);
-		getContentPane().add(idaVuelta);
-		getContentPane().add(soloIda);
-		getContentPane().add(viajesProgramados);
-		add(lblNewLabel1);
+		p2.add(lblNewLabel1);
 		lblNewLabel1.setVisible(false);
-		add(lblNewLabel2);
+		p2.add(lblNewLabel2);
 		lblNewLabel2.setVisible(false);
-		add(lblNewLabel3);
+		p2.add(lblNewLabel3);
 		lblNewLabel3.setVisible(false);
 		
 		
+		setTitle("Billetes");
 		pack();
 		setSize(700,450);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	
-	
 	
 	public static void main(String[] args) {
 		new VentanaViaje();
