00b4f50235c0e96e129c0c7ae27ff45ca77ab6fe
andreamartinez21 <andreamartinez21@opendeusto.es>
2021-10-29 13:30:34 +0200

    VentanaGaleria botones creados y unida con VentanaGaleriaActividades

--- src/ventana/VentanaGaleria.java
+++ src/ventana/VentanaGaleria.java
@@ -6,6 +6,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaGaleria extends JFrame{
 
@@ -21,15 +23,30 @@ public class VentanaGaleria extends JFrame{
 
 		int numFotos = 6; // calculará el número de fotos que haya en la bd
         
-        panelPrincipal = new JPanel(new GridLayout(numFotos/2, 2));
+        panelPrincipal = new JPanel(new GridLayout(numFotos, 2));
+        //panelPrincipal = new JPanel(new GridLayout(0, 1));
         panelPrincipal.setSize(1000, 2000);
         
         for (int i = 1; i <= numFotos; i++) {
             BufferedImage bufferedImage = ImageIO.read(new File("images/barco" +i+ ".jpg"));
             Image image = bufferedImage.getScaledInstance(400, 200, Image.SCALE_DEFAULT);
-		    JLabel label = new JLabel(new ImageIcon(image));
-            //label.setSize(450, 250);
+		    //JLabel label = new JLabel(new ImageIcon(image));
+            JButton boton = new JButton(new ImageIcon(image));
+            JLabel label = new JLabel("hola"); //bd.getActividades
+            //boton.setPreferredSize(new Dimension(200, 100));
+			
+			//b.setSize(TAMANYO_BOTON);
+
+            panelPrincipal.add(boton);
             panelPrincipal.add(label);
+
+            boton.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					VentanaGaleriaActividades v = new VentanaGaleriaActividades();
+				}
+			});
         }
 
         scroll = new JScrollPane(panelPrincipal);
--- src/ventana/VentanaGaleriaActividades.java
+++ src/ventana/VentanaGaleriaActividades.java
@@ -1,5 +1,34 @@
 package ventana;
 
-public class VentanaGaleriaActividades {
-    
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaGaleriaActividades extends JFrame{
+
+    private Container cp;
+    private JPanel panelPrincipal;
+
+    public VentanaGaleriaActividades(){
+
+        cp = this.getContentPane();
+        panelPrincipal = new JPanel(new GridLayout(2, 2));
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+
+    }
+
+    public static void main(String[] args) {
+		new VentanaGaleriaActividades();
+
+	}
+
+
 }
