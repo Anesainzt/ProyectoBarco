f7c004068b646bb478b918a8ae85b3dc4152af52
andreamartinez21 <andreamartinez21@opendeusto.es>
2021-11-28 20:22:05 +0100

    VentanaGaleria con paneles, VentanaGaleriaActividades eliminada

--- src/ventana/VentanaGaleria.java
+++ src/ventana/VentanaGaleria.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.System.Logger.Level;
 import java.util.ArrayList;
 import java.util.List;
 import java.awt.event.ActionListener;
@@ -18,9 +19,14 @@ import clases.Surf;
 
 public class VentanaGaleria extends JFrame{
 
-	private Container cp;
-    private JPanel panelPrincipal;
+    private JPanel contentPane;
     private JScrollPane scroll;
+    private JPanel panel1;
+
+    private JPanel panel2;
+
+    private String nombre = "SURF";
+    private String instructor = "Manolo";
 
     private List<Surf> listaSurf;
     private List<Ski> listaSki;
@@ -32,10 +38,15 @@ public class VentanaGaleria extends JFrame{
 
         BD bd = new BD();
         bd.connect();
-        
-        cp = this.getContentPane();
-		this.setTitle("Galería de fotos");
-        this.setSize(1000, 600);
+
+        setTitle("Galería de fotos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(1000, 600));
+        setVisible(true);
+        pack();
+
+        contentPane = new JPanel();
+        setContentPane(contentPane);
 
         listaSurf = new ArrayList<Surf>(bd.getListaSurf());
         listaSki = new ArrayList<Ski>(bd.getListaSki());
@@ -47,10 +58,12 @@ public class VentanaGaleria extends JFrame{
 
         System.out.println(listaSurf);
 
+        // panel 1
+
 		int numFotos = 6; // calculará el número de fotos que haya en la bd
         
-        panelPrincipal = new JPanel(new GridLayout(numFotos, 2));
-        panelPrincipal.setSize(1000, 2000);
+        panel1 = new JPanel(new GridLayout(numFotos, 2));
+        panel1.setSize(1000, 2000);
         
         for (int i = 1; i <= numFotos; i+=2) {
             int j = i + 1;
@@ -65,18 +78,19 @@ public class VentanaGaleria extends JFrame{
             JLabel label2 = new JLabel("hola"); //bd.getActividades
             //boton.setPreferredSize(new Dimension(200, 100));
 
-            panelPrincipal.add(boton);
-            panelPrincipal.add(boton2);
-            panelPrincipal.add(label);
-            panelPrincipal.add(label2);
+            panel1.add(boton);
+            panel1.add(boton2);
+            panel1.add(label);
+            panel1.add(label2);
 
             boton.addActionListener(new ActionListener() {
 				
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					try {
-                        new VentanaGaleriaActividades();
-                    } catch (IOException e) {
+                        scroll.setVisible(false);
+                        panel2.setVisible(true);
+                    } catch (Exception e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
@@ -88,8 +102,9 @@ public class VentanaGaleria extends JFrame{
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					try {
-                        new VentanaGaleriaActividades();
-                    } catch (IOException e) {
+                        scroll.setVisible(false);
+                        panel2.setVisible(true);
+                    } catch (Exception e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
@@ -97,13 +112,62 @@ public class VentanaGaleria extends JFrame{
 			});
         }
 
-        scroll = new JScrollPane(panelPrincipal);
+        scroll = new JScrollPane(panel1);
         scroll.setVisible(true);
-        this.add(scroll);
 
-        //this.pack();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        // panel 2
+
+        panel2 = new JPanel(new GridLayout(4, 2));
+        panel2.setSize(1000, 600);
+
+        JLabel labelNombre = new JLabel(nombre);
+        labelNombre.setFont(new Font("Serif", Font.PLAIN, 55));
+        JLabel labelCodigo = new JLabel("Código");
+        JLabel labelAforo = new JLabel("Aforo");
+        JLabel labelInstructor = new JLabel("Instructor: " +instructor);
+        JLabel labelUbicacion = new JLabel("Ubicación");
+        JLabel labelDescripcion = new JLabel("Descripción");
+        JLabel labelCantidadMaterial= new JLabel("Cantidad material");
+
+        JButton botonAtras = new JButton("Atrás");
+
+        BufferedImage bufferedImage = ImageIO.read(new File("images/barco2.jpg"));
+        Image image = bufferedImage.getScaledInstance(400, 200, Image.SCALE_DEFAULT);
+		JLabel labelImagen = new JLabel(new ImageIcon(image));
+
+        panel2.add(labelNombre);
+        panel2.add(labelCodigo);
+        panel2.add(labelAforo);
+        panel2.add(labelInstructor);
+        panel2.add(labelUbicacion);
+        panel2.add(labelDescripcion);
+        panel2.add(labelCantidadMaterial);
+        panel2.add(botonAtras);
+
+        botonAtras.addActionListener(new ActionListener() {
+				
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    scroll.setVisible(true);
+                    panel2.setVisible(false);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                	//logger.log(Level.INFO, "");//METER LA INFO DEL ERROR
+                }
+            }
+        });
+
+        panel2.add(labelImagen);
+
+        //
+
+        contentPane.setVisible(true);
+        panel1.setVisible(true);
+        panel2.setVisible(false);
+
+        contentPane.add(scroll);
+        contentPane.add(panel2);
     }
 
     public static void main(String[] args) {
