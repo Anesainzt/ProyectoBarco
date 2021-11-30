4c6eca1f4386560d556e9b5a7dc83e8f8baeb349
Ane Sainz Tabares <anesainzt@opendeusto.es>
2021-11-30 19:18:44 +0100

    Mejorada ventana viaje

--- src/ventana/VentanaViaje.java
+++ src/ventana/VentanaViaje.java
@@ -1,38 +1,24 @@
 package ventana;
 
+
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.util.*;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 
 import clases.Usuario;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.awt.event.ActionEvent;
-import javax.swing.JScrollPane;
-import javax.swing.JList;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-
 public class VentanaViaje extends JFrame {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	
+	Container cp;
 	JPanel contentPane;
 	JDateChooser calendarioIda, calendarioVuelta;
 	static JDateChooser calendarioActividades;
@@ -43,21 +29,16 @@ public class VentanaViaje extends JFrame {
 	JList<String> listaViajes;
 	JScrollPane scrollListaViajes;
 	
+	JMenuBar menuBar;
+	JMenu menu;
+	JMenuItem itemPerfil, itemGaleria, itemSesion, itemSalir;
 	
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaViaje frame = new VentanaViaje(null);
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		new VentanaViaje(null);
 	}
-
+	
 	public VentanaViaje(Usuario uActual) {
+				
 		sdf = new SimpleDateFormat("dd-MM-yyyy");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 510);
@@ -66,11 +47,90 @@ public class VentanaViaje extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
+		
+		//menú
+		menuBar = new JMenuBar();
+		menu = new JMenu("Más opciones");
+	
+		itemPerfil = new JMenuItem("Mi perfil", new ImageIcon("images/perfil.png"));
+		itemPerfil.setBackground(Color.WHITE);
+		itemPerfil.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new VentanaPerfil(uActual);
+				dispose();
+				
+			}
+		});
+		
+		itemGaleria = new JMenuItem("Galería", new ImageIcon("images/galeria.png"));
+		itemGaleria.setBackground(Color.WHITE);
+		itemGaleria.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					new VentanaGaleria();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+
+		itemSesion = new JMenuItem("Cerrar Sesión", new ImageIcon("images/sesion.png"));
+		itemSesion.setBackground(Color.WHITE);
+		itemSesion.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					JOptionPane.showMessageDialog(null, "¡Hasta pronto!");
+					new VentanaInicio();
+					dispose();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		itemSalir = new JMenuItem("Salir", new ImageIcon("images/salir.png"));
+		itemSalir.setBackground(Color.WHITE);
+		itemSalir.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					JOptionPane.showMessageDialog(null, "¡Nos vemos pronto!");
+					dispose();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+
+		
+		setJMenuBar(menuBar);
+		menuBar.add(menu);
+		menu.add(itemPerfil);
+		menu.add(itemGaleria);
+		menu.add(itemSesion);
+		menu.add(itemSalir);
+		
 		JPanel panelTitulo = new JPanel();
 		contentPane.add(panelTitulo, BorderLayout.NORTH);
 		
+		
+		JPanel panel_3 = new JPanel();
+		panelTitulo.add(panel_3);
+		
 		JLabel lblTitulo = new JLabel("TU VIAJE EMPIEZA AQUÍ");
-		panelTitulo.add(lblTitulo);
+		panel_3.add(lblTitulo);
 		
 		JComboBox<String> cbOrigen = new JComboBox<String>();
 		cbOrigen.addItem("Bilbao");
@@ -180,6 +240,9 @@ public class VentanaViaje extends JFrame {
 		JButton btnIraActividad = new JButton("Aceptar billete");
 		panel_1.add(btnIraActividad);
 		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2, BorderLayout.SOUTH);
+		
 		calendarioActividades = new JDateChooser();
 		cact = calendarioActividades.getJCalendar();
 		
@@ -208,5 +271,5 @@ public class VentanaViaje extends JFrame {
 		setVisible(true);
 		
 	}
-
+	
 }
